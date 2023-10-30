374ba295d2161debf001fe48d9ac1b639ed09187
rockclimber147 <daylencsmith@gmail.com>
2023-10-30 14:21:33 -0700

    Make reusable elements easier to identify

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function (){
     // Load bottom navbar to page
     $.get("./reusable html/bottom_nav.html", function (data) {
-        $('#footer').html(data);
+        $('#reusable_footer').html(data);
     });
     // load top navbar to page
     $.get("./reusable html/top_nav.html", function (data) {
