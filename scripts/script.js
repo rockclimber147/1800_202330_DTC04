5ab01b8ea91704a069c55d07b951c01fa96f1780
rockclimber147 <daylencsmith@gmail.com>
2023-10-30 14:14:38 -0700

    Comment code

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function (){
+    // Load bottom navbar to page
     $.get("./reusable html/bottom_nav.html", function (data) {
         $('#footer').html(data);
     });
+    // load top navbar to page
     $.get("./reusable html/top_nav.html", function (data) {
         $('#header').html(data);
     });
