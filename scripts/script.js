bb26947376623ea2a04ca3353e3c656f9fcffd1b
rockclimber147 <daylencsmith@gmail.com>
2023-10-30 14:12:24 -0700

    Add reusable headwer

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,7 @@ $(document).ready(function (){
     $.get("./reusable html/bottom_nav.html", function (data) {
         $('#footer').html(data);
     });
+    $.get("./reusable html/top_nav.html", function (data) {
+        $('#header').html(data);
+    });
 })
