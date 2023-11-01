480e2f3cbf83feaa2a580bec4c4cd3d1c0152756
rockclimber147 <daylencsmith@gmail.com>
2023-11-01 12:27:11 -0700

    Reformat html headers

--- scripts/script.js
+++ scripts/script.js
@@ -53,3 +53,15 @@ $(document).ready(function () {
         return Math.floor(Math.random() * max);
     }
 })
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+    }).catch((error) => {
+        // An error happened.
+    });
+}
