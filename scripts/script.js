3ba76b9735142a757886f3e13a200c87e2dabdeb
yoshidont-mind <tyoshida6@my.bcit.ca>
2023-11-04 11:25:00 -0700

    CHANGE class for tags to make all tags align-left

--- scripts/script.js
+++ scripts/script.js
@@ -24,19 +24,19 @@ $(document).ready(function () {
     function generateRandomQuestCard() {
         return `<div class="card w-100 mb-2 container-fluid p-sm-3">
             <div class="row">
-                <div class="card-body col-8">
+                <div class="card_body col-8 p-2">
                     <h5 class="card-title">Quest_Name</h5>
                     <div class="row card-properties px-2">
-                        <div class="col-5 px-0 py-1">${'★'.repeat((getRandomInt(5) + 1))}</div>
-                        <div class="col-3 px-2 py-1">${'$'.repeat((getRandomInt(4) + 1))}</div>
-                        <div class="col-4 px-2 py-1">${(Math.random() * 50).toFixed(1)} mi</div>
+                        <div class="col-5 px-1 py-1">${'★'.repeat((getRandomInt(5) + 1))}</div>
+                        <div class="col-3 px-2 py-1">${'$'.repeat((getRandomInt(3) + 1))}</div>
+                        <div class="col-4 px-2 py-1">${(Math.random() * 10).toFixed(1)} mi</div>
                     </div>
-                    <div class="row card-properties px-2">
-                        ${generateTags(getRandomInt(4) + 1)}
+                    <div class="justify-content-start card_properties">
+                        ${generateTags(getRandomInt(3) + 1)}
                     </div>
                 </div>
                 <div class="img-container col-4 d-flex align-items-center">
-                    <img class="w-100" src="https://picsum.photos/id/${getRandomInt(255)}/100/100" alt="">
+                    <img class="w-100" src="https://picsum.photos/id/${getRandomIntBetween(200, 255)}/100/100" alt="">
                 </div>
             </div>
         </div>`
@@ -54,7 +54,7 @@ $(document).ready(function () {
         }
         tagString = ""
         for (i = 0; i < tagIDs.length; i++) {
-            tagString += `<button class="col rounded p-1 me-1 border-0 card-tag">${questTags[tagIDs[i]]}</button>`;
+            tagString += `<button class="d-inline-block rounded px-2 m-1 border-0 card_tag">${questTags[tagIDs[i]]}</button>`;
         }
         return tagString;
     }
