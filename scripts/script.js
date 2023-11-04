ecf9229dc72cb36a938aa8706894103ba3b6dc2a
yoshidont-mind <tyoshida6@my.bcit.ca>
2023-11-04 16:18:22 -0700

    fix bug

--- scripts/script.js
+++ scripts/script.js
@@ -35,27 +35,27 @@ function log_tags() {
         });
 }
 
-function writeQuests() {
-    //define a variable for the collection you want to create in Firestore to populate data
-    var questRef = db.collection("quests");
+// function writeQuests() {
+//     //define a variable for the collection you want to create in Firestore to populate data
+//     var questRef = db.collection("quests");
 
-    questRef.add({
-        quest_name: "Let's explore a tiny Japan!",
-        location_name: "Nitobe Memorial Garden",
-        location: [49.26645018574508, -123.25968546090411],
-        rate: 3,
-        cost: 2,
-        description: "The Nitobe Memorial Garden is a 2.5 acre (one hectare) traditional Japanese garden located at the University of British Columbia, just outside the city limits of Vancouver, British Columbia, Canada. Although it is part of the UBC Botanical Garden, Nitobe Memorial Garden is located next to UBC's Asian Centre, two kilometers from the main UBC Botanical Garden.",
-        tag_ids: ["D4NeTY30U09lD3TemV4Y"],
-    })
+//     questRef.add({
+//         quest_name: "Let's explore a tiny Japan!",
+//         location_name: "Nitobe Memorial Garden",
+//         location: [49.26645018574508, -123.25968546090411],
+//         rate: 3,
+//         cost: 2,
+//         description: "The Nitobe Memorial Garden is a 2.5 acre (one hectare) traditional Japanese garden located at the University of British Columbia, just outside the city limits of Vancouver, British Columbia, Canada. Although it is part of the UBC Botanical Garden, Nitobe Memorial Garden is located next to UBC's Asian Centre, two kilometers from the main UBC Botanical Garden.",
+//         tag_ids: ["D4NeTY30U09lD3TemV4Y"],
+//     })
 
-    questRef.add({
-        quest_name: "",
-        location_name: "",
-        location: [,],
-        rate: ,
-        cost: ,
-        description: ,
-        tag_ids: [""],
-    })
-}
+//     questRef.add({
+//         quest_name: "",
+//         location_name: "",
+//         location: [,],
+//         rate: ,
+//         cost: ,
+//         description: ,
+//         tag_ids: [""],
+//     })
+// }
