fb4988e6487e0370cedc86d7423e7ed8fadebd01
yoshidont-mind <tyoshida6@my.bcit.ca>
2023-11-04 16:13:08 -0700

    add data of quest

--- scripts/script.js
+++ scripts/script.js
@@ -27,12 +27,12 @@ function logout() {
 
 function log_tags() {
     db.collection('tags').get()
-    .then(all_tags => {
-        all_tags.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data().tag_name);
-    })
-    });
+        .then(all_tags => {
+            all_tags.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data().tag_name);
+            })
+        });
 }
 
 function writeQuests() {
@@ -40,33 +40,13 @@ function writeQuests() {
     var questRef = db.collection("quests");
 
     questRef.add({
-        quest_name: "",
-        location_name: "",
-        location: [,],
-        rate: ,
-        cost: ,
-        description: ,
-        tag_ids: [""],
-    })
-
-    questRef.add({
-        quest_name: "",
-        location_name: "",
-        location: [,],
-        rate: ,
-        cost: ,
-        description: ,
-        tag_ids: [""],
-    })
-
-    questRef.add({
-        quest_name: "",
-        location_name: "",
-        location: [,],
-        rate: ,
-        cost: ,
-        description: ,
-        tag_ids: [""],
+        quest_name: "Let's explore a tiny Japan!",
+        location_name: "Nitobe Memorial Garden",
+        location: [49.26645018574508, -123.25968546090411],
+        rate: 3,
+        cost: 2,
+        description: "The Nitobe Memorial Garden is a 2.5 acre (one hectare) traditional Japanese garden located at the University of British Columbia, just outside the city limits of Vancouver, British Columbia, Canada. Although it is part of the UBC Botanical Garden, Nitobe Memorial Garden is located next to UBC's Asian Centre, two kilometers from the main UBC Botanical Garden.",
+        tag_ids: ["D4NeTY30U09lD3TemV4Y"],
     })
 
     questRef.add({
