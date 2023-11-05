582d213530b7d979b55e33bdd258b81b25932f5c
Daylen Smith <daylencsmith@gmail.com>
2023-11-04 20:00:10 -0700

    Conect skeleton.js to appropriate reusable html

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     // Load bottom navbar to page
-    $.get("./reusable html/bottom_nav.html", function (data) {
-        $('#reusable_footer').html(data);
-    });
-    // load top navbar to page
-    $.get("./reusable html/top_nav.html", function (data) {
-        $('#reusable_header').html(data);
-    });
+    // $.get("./reusable html/bottom_nav.html", function (data) {
+    //     $('#reusable_footer').html(data);
+    // });
+    // // load top navbar to page
+    // $.get("./reusable html/top_nav.html", function (data) {
+    //     $('#reusable_header').html(data);
+    // });
 })
 
 //------------------------------------------------
--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -49,12 +49,12 @@ function loadSkeleton() {
             //if the pointer to "user" object is not null, then someone is logged in
             // User is signed in.
             // Do something for the user here.
-            console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#reusable_header').load('./reusable_html/top_nav_after_login.html'));
+            console.log($('#reusable_footer').load('./reusable_html/bottom_nav.html'));
         } else {
             // No user is signed in.
-            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
-            console.log($('#footerPlaceholder').load('./text/footer.html'));
+            console.log($('#reusable_header').load('./reusable_html/top_nav_before_login.html'));
+            console.log($('#reusable_footer').load('./reusable_html/bottom_nav.html'));
         }
     });
 }
