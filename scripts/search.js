f93cee74a34f6cddc823b2592f1e82b71472f7af
Daylen Smith <daylencsmith@gmail.com>
2023-11-11 15:11:59 -0800

    Modify update_quest_cards function to load from db passes as argument

--- scripts/search.js
+++ scripts/search.js
@@ -32,6 +32,7 @@ $(document).ready(function () {
 
             quest_db = await db.collection('quests').get() 
             update_map(quest_db);
+            update_quest_cards(quest_db);
 
             tag_db = await db.collection('tags').get()                                                        // get all tags
             tag_db.forEach(tag_doc => {
@@ -225,42 +226,39 @@ $(document).ready(function () {
                   });
       }
 
-      function update_quest_cards() {
-            quest_collection.get()                                       // the collection called "quests"
-                  .then(all_quests => {
-                        all_quests.forEach(doc => {                          // iterate through each doc and for each:
-                              var quest_name = doc.data().quest_name;          // get the quest name
-                              var quest_rating = doc.data().rate;              // get value of the "details" key
-                              var quest_price = doc.data().cost;               // get the price of the quest
-                              var image_name = doc.data().image_name;          // get the name of the image
-                              var quest_description = doc.data().description;  // gets the description field (TODO)
-                              var quest_location = doc.data().location;
-                              var quest_distance = calculateDistance(user_location, quest_location);
-                              var quest_tag_id_list = doc.data().tag_ids       // get the list of tag ids
-                              var quest_id = doc.id;                           // get the quest ids
+      function update_quest_cards(quest_db) {
+            quest_db.forEach(doc => {                          // iterate through each doc and for each:
+                  var quest_name = doc.data().quest_name;          // get the quest name
+                  var quest_rating = doc.data().rate;              // get value of the "details" key
+                  var quest_price = doc.data().cost;               // get the price of the quest
+                  var image_name = doc.data().image_name;          // get the name of the image
+                  var quest_description = doc.data().description;  // gets the description field (TODO)
+                  var quest_location = doc.data().location;
+                  var quest_distance = calculateDistance(user_location, quest_location);
+                  var quest_tag_id_list = doc.data().tag_ids       // get the list of tag ids
+                  var quest_id = doc.id;                           // get the quest ids
 
-                              // Clone the contents of the quest card template element (not the parent template element)
-                              let new_quest_card = $(quest_card_node).children().clone();
+                  // Clone the contents of the quest card template element (not the parent template element)
+                  let new_quest_card = $(quest_card_node).children().clone();
 
-                              //update title and text and image
-                              new_quest_card.find('.quest_name').text(quest_name);
-                              new_quest_card.find('.quest_rating').text('★'.repeat(quest_rating));
-                              new_quest_card.find('.quest_price').text('$'.repeat(quest_price));
-                              new_quest_card.find('.quest_description').text(quest_description);
-                              new_quest_card.find('.quest_distance').text(quest_distance + 'km');
-                              new_quest_card.find('.quest_image').attr('src', `./images/${image_name}.jpg`); // find image and put in new quest card
-                              new_quest_card.find('.quest_detail_link').attr('href', `./quest-detail.html?quest_id=${quest_id}`); // set links to quest cards
+                  //update title and text and image
+                  new_quest_card.find('.quest_name').text(quest_name);
+                  new_quest_card.find('.quest_rating').text('★'.repeat(quest_rating));
+                  new_quest_card.find('.quest_price').text('$'.repeat(quest_price));
+                  new_quest_card.find('.quest_description').text(quest_description);
+                  new_quest_card.find('.quest_distance').text(quest_distance + 'km');
+                  new_quest_card.find('.quest_image').attr('src', `./images/${image_name}.jpg`); // find image and put in new quest card
+                  new_quest_card.find('.quest_detail_link').attr('href', `./quest-detail.html?quest_id=${quest_id}`); // set links to quest cards
 
-                              if (quest_tag_id_list[0] != "") {
-                                    for (let i = 0; i < quest_tag_id_list.length; i++) {
-                                          let new_quest_tag = $(quest_tag_node).children().clone();
-                                          new_quest_tag.text(all_quest_tags[quest_tag_id_list[i]]);
-                                          new_quest_tag.appendTo(new_quest_card.find('.quest_tags_container'));
-                                    }
-                              }
-                              new_quest_card.appendTo('#quest_cards_go_here')
-                        })
-                  })
+                  if (quest_tag_id_list[0] != "") {
+                        for (let i = 0; i < quest_tag_id_list.length; i++) {
+                              let new_quest_tag = $(quest_tag_node).children().clone();
+                              new_quest_tag.text(all_quest_tags[quest_tag_id_list[i]]);
+                              new_quest_tag.appendTo(new_quest_card.find('.quest_tags_container'));
+                        }
+                  }
+                  new_quest_card.appendTo('#quest_cards_go_here')
+            })
       }
 
       function autocomplete(inp, arr) {
