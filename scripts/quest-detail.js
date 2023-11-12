da4cb7e6f69421f3228f41b2610c263fa6d20edb
yoshidont-mind <tyoshida6@my.bcit.ca>
2023-11-11 21:49:12 -0800

    Complete JS for pop-ups

--- scripts/quest-detail.js
+++ scripts/quest-detail.js
@@ -18,22 +18,9 @@ function display_quest_info() {
             $(`.quest_place`).text(quest_place);
             $('.quest_price').text('$'.repeat(quest_price));
             $(`.quest_description`).text(quest_description);
-            $(`.quest_complete_button`).attr('onclick', `location.href='./quest-completion.html?quest_id=${ID}'`);
+            $(`#complete_quest_pop_up .pop_up_confirm_button`).attr('onclick', `location.href='./quest-completion.html?quest_id=${ID}'`);
         })
 }
-
-// function switch_buttons() {
-//     $('.quest_accept_button').click(function() {
-//         $(this).addClass('d-none')
-//         $('.quest_complete_button, .quest_drop_button').removeClass('d-none');
-//     });
-
-//     $('.quest_drop_button').click(function() {
-//         $(this).addClass('d-none');
-//         $('.quest_complete_button').addClass('d-none');
-//         $('.quest_accept_button').removeClass('d-none');
-//     })
-// }
 
 function switch_buttons_and_pop_ups() {
     $('.quest_accept_button').click(function() {
@@ -46,13 +33,33 @@ function switch_buttons_and_pop_ups() {
         $('.quest_complete_button, .quest_drop_button').removeClass('d-none');
         $('#details_go_here').removeClass('opacity-25');
     })
+    $('.quest_drop_button').click(function () {
+        $('#drop_quest_pop_up').removeClass('d-none');
+        $('#details_go_here').addClass('opacity-25');
+    })
+    $('#drop_quest_pop_up .pop_up_cancel_button').click(function () {
+        $(this).closest('.pop_up').addClass('d-none');
+        $('#details_go_here').removeClass('opacity-25');
+    })
+    $('#drop_quest_pop_up .pop_up_confirm_button').click(function () {
+        $(this).closest('.pop_up').addClass('d-none');
+        $('#quest_dropped_pop_up').removeClass('d-none');
+    })
+    $('#quest_dropped_pop_up .pop_up_close_button').click(function () {
+        $(this).closest('.pop_up').addClass('d-none');
+        $('.quest_accept_button').removeClass('d-none');
+        $('.quest_complete_button, .quest_drop_button').addClass('d-none');
+        $('#details_go_here').removeClass('opacity-25');
+    })
     $('.quest_complete_button').click(function() {
         $('#complete_quest_pop_up').removeClass('d-none');
         $('#details_go_here').addClass('opacity-25');
     })
-    $('#complete_quest_pop_up .pop_up_close_button').click(function() {
+    $('#complete_quest_pop_up .pop_up_cancel_button').click(function() {
         $(this).closest('.pop_up').addClass('d-none');
+        $('#details_go_here').removeClass('opacity-25');
     })
+
 }
 
 $(document).ready(function() {
