1668e7a66419b3996ddeadc91679173306fb5592
IRENE CHEUNG <icheung24@my.bcit.ca>
2023-11-14 22:44:49 -0800

    Make stars clickable

--- scripts/review.js
+++ scripts/review.js
@@ -18,3 +18,21 @@ function getQuestName(id) {
 }
 
 getQuestName(questDocID);
+
+// Add this JavaScript code to make stars clickable
+
+// Select all elements with the class name "star" and store them in the "stars" variable
+const stars = document.querySelectorAll('.star');
+
+// Demo 10 Step 2.7
+// Iterate through each star element
+stars.forEach((star, index) => {
+      // Add a click event listener to the current star
+      star.addEventListener('click', () => {
+            // Fill in clicked star and stars before it
+            for (let i = 0; i <= index; i++) {
+                  // Change the text content of stars to 'star' (filled)
+                  document.getElementById(`star${i + 1}`).textContent = 'star';
+            }
+      });
+});
