3049926663c67e76b22d400a7fe608a2e9dad38e
IRENE CHEUNG <icheung24@my.bcit.ca>
2023-11-14 23:00:29 -0800

    Add writeReview function

--- scripts/review.js
+++ scripts/review.js
@@ -36,3 +36,56 @@ stars.forEach((star, index) => {
             }
       });
 });
+
+function writeReview() {
+      console.log("inside write review");
+      let questTitle = document.getElementById("title").value;
+      let questLevel = document.getElementById("level").value;
+      let questDescription = document.getElementById("description").value;
+
+      // Get the star rating
+      // Get all the elements with the class "star" and store them in the 'stars' variable
+      const stars = document.querySelectorAll('.star');
+      const money = document.querySelectorAll('.money')
+      // Initialize a variable 'questRating' to keep track of the rating count
+      let questRating = 0;
+      let questCost = 0;
+      // Iterate through each element in the 'stars' NodeList using the forEach method
+      stars.forEach((star) => {
+            // Check if the text content of the current 'star' element is equal to the string 'star'
+            if (star.textContent === 'star') {
+                  // If the condition is met, increment the 'questRating' by 1
+                  questRating++;
+            }})
+      money.forEach((money) => {
+            // Check if the text content of the current 'star' element is equal to the string 'star'
+            if (money.textContent === 'attach_money') {
+                  // If the condition is met, increment the 'questRating' by 1
+                  questCost++;
+            }
+      });
+
+      console.log(questTitle, questLevel, questDescription, questRating, questCost);
+
+      var user = firebase.auth().currentUser;
+      if (user) {
+            var currentUser = db.collection("users").doc(user.uid);
+            var userID = user.uid;
+
+            // Get the document for the current user.
+            db.collection("reviews").add({
+                  questDocID: questDocID,
+                  userID: userID,
+                  title: questTitle,
+                  level: questLevel,
+                  description: questDescription,
+                  rating: questRating, // Include the rating in the review
+                  timestamp: firebase.firestore.FieldValue.serverTimestamp()
+            }).then(() => {
+                  window.location.href = "thanks.html"; // Redirect to the thanks page
+            });
+      } else {
+            console.log("No user is signed in");
+            window.location.href = 'review.html';
+      }
+}
