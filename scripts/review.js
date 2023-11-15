b12d19728a93dd18b8962131ea632d43ed947911
IRENE CHEUNG <icheung24@my.bcit.ca>
2023-11-14 23:16:39 -0800

    Add populate review function

--- scripts/review.js
+++ scripts/review.js
@@ -102,3 +102,59 @@ function writeReview() {
             window.location.href = 'review.html';
       }
 }
+
+// Demo 10 - Populating reviews
+
+function populateReviews() {
+      console.log("test");
+      let questCardTemplate = document.getElementById("reviewCardTemplate");
+      let questCardGroup = document.getElementById("reviewCardGroup");
+
+      let params = new URL(window.location.href); // Get the URL from the search bar
+      let quest_id = params.searchParams.get("quest_id");
+
+      // Double-check: is your collection called "Reviews" or "reviews"?
+      db.collection("reviews")
+            .where("questDocID", "==", quest_id)
+            .get()
+            .then((allReviews) => {
+                  reviews = allReviews.docs;
+                  console.log(reviews);
+                  reviews.forEach((doc) => {
+                        var title = doc.data().title;
+                        var level = doc.data().level;
+                        var description = doc.data().description;
+                        var time = doc.data().timestamp.toDate();
+                        var rating = doc.data().rating; // Get the rating value
+                        console.log(rating)
+
+                        console.log(time);
+
+                        let reviewCard = questCardTemplate.content.cloneNode(true);
+                        reviewCard.querySelector(".title").innerHTML = title;
+                        reviewCard.querySelector(".time").innerHTML = new Date(
+                              time
+                        ).toLocaleString();
+                        reviewCard.querySelector(".level").innerHTML = `Level: ${level}`;
+                        reviewCard.querySelector(".description").innerHTML = `Description: ${description}`;
+
+                        // Populate the star rating based on the rating value
+
+                        // Initialize an empty string to store the star rating HTML
+                        let starRating = "";
+                        // This loop runs from i=0 to i<rating, where 'rating' is a variable holding the rating value.
+                        for (let i = 0; i < rating; i++) {
+                              starRating += '<span class="material-icons">star</span>';
+                        }
+                        // After the first loop, this second loop runs from i=rating to i<5.
+                        for (let i = rating; i < 5; i++) {
+                              starRating += '<span class="material-icons">star_outline</span>';
+                        }
+                        reviewCard.querySelector(".star-rating").innerHTML = starRating;
+
+                        questCardGroup.appendChild(reviewCard);
+                  });
+            });
+}
+
+populateReviews();
