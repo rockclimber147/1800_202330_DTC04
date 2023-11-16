e8d51d87001f843ec9df25fc74215b901a29fba5
yoshidont-mind <tyoshida6@my.bcit.ca>
2023-11-16 00:12:14 -0800

    Update writeQuests()

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -82,13 +82,13 @@ async function write_quest_ids_to_tags() {               // Async function becau
 async function update_quest_name_list() {
     quest_name_list = []
     all_quests = await db.collection("quests").get();
-    all_quests.forEach((quest_doc) =>{
+    all_quests.forEach((quest_doc) => {
         quest_name_list.push(quest_doc.data().quest_name)
     })
     console.log(quest_name_list)
     let quest_names_doc_id;
     quest_names_doc = await db.collection("quest_names").get();
-    quest_names_doc.forEach((quest_name_list_doc)=>{             
+    quest_names_doc.forEach((quest_name_list_doc) => {
         quest_names_doc_id = quest_name_list_doc.id;
     })
 
@@ -98,26 +98,26 @@ async function update_quest_name_list() {
 /**
  * This adds search keywords to each quest composed of its tags and the words in the title, ALL LOWERCASE
  */
-async function add_keywords_to_quests(){
+async function add_keywords_to_quests() {
     let quest_collection = await db.collection("quests").get(); // get quest collection
     let tag_collection = await db.collection("tags").get(); // get tag collection
     tag_id_map = {};
 
-    tag_collection.forEach((tag_doc)=>{                  // populate dictionary
+    tag_collection.forEach((tag_doc) => {                  // populate dictionary
         console.log(tag_doc.data().tag_name)
         tag_id_map[tag_doc.id] = tag_doc.data().tag_name;
     })
     console.log(tag_id_map)
 
-    quest_collection.forEach((quest_doc)=>{
+    quest_collection.forEach((quest_doc) => {
         keyword_list = quest_doc.data().quest_name.toLowerCase().split(' '); // break up quest name into lowercase words and store as a list
-        if (quest_doc.data().tag_ids[0] != ''){
-            for (let i = 0; i < quest_doc.data().tag_ids.length; i++){
+        if (quest_doc.data().tag_ids[0] != '') {
+            for (let i = 0; i < quest_doc.data().tag_ids.length; i++) {
                 keyword_list.push(tag_id_map[quest_doc.data().tag_ids[i]].toLowerCase()) // get the tag name from the tag id, make it lowercase and add it to the keyword list
             }
         }
         console.log('keyword list:', keyword_list);
-        db.collection('quests').doc(quest_doc.id).update({'keywords': keyword_list})
+        db.collection('quests').doc(quest_doc.id).update({ 'keywords': keyword_list })
     })
 }
 
@@ -126,46 +126,50 @@ function writeQuests() {
     var questRef = db.collection("quests");
 
     questRef.add({
-        quest_name: "Reveal mysteries of human beings!",
-        location_name: "Museum of Anthropology",
-        location: [49.26961348051608, -123.25948381904506],
-        rate: 5, //integer between 1 to 5
-        cost: 2, // integer between 1 to 3
-        description: "The Museum of Anthropology at the University of British Columbia (UBC) campus in Vancouver, British Columbia, Canada displays world arts and cultures, in particular works by First Nations of the Pacific Northwest. As well as being a major tourist destination, MOA is a research and teaching museum, where UBC courses in art, anthropology, archaeology, conservation, and museum studies are given. MOA houses close to 50,000 ethnographic objects, as well as 535,000 archaeological objects in its building alone.",
-        tag_ids: ["j6MrxNTQVsVDqZZqKY3A"],
-        image_name: "Museum of Anthropology",
+        quest_name: "Walk among trees and find the secret viewpoints!",
+        location_name: "Greenheart Treewalk",
+        location: [49.25077224296747, -123.24653281646229], // copy from Google map
+        rate: 2, //integer between 1 to 5 (decide as you want!)
+        cost: 1, // integer between 1 to 3 (1: ~$15, 2: ~$40, 3: $40~)
+        description: "The Greenheart TreeWalk will spark your adventurous spirit as you navigate suspended walkways and tree platforms high above the forest ﬂoor. Located in the heart of UBC Botanical Garden, the 310 metre-long tree top canopy walkway hangs from huge Douglas firs, cedars and grand firs, many of which are over 100 years old. Reaching a height of nearly 20 metres above the forest floor, the walkway will give you a bird’s eye view of Vancouver’s magnificent coastal temperate rainforest.",
+        tag_ids: ["D4NeTY30U09lD3TemV4Y", "ntk5nt0wadhVx4A17Weu"], // uncomment helper.js at main.html and run log_tags() to see the list of tags (or you can copy tag_ids from firebase)
+        image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Greenheart_TreeWalk_-_UBC_Botanical_Garden_-_Vancouver%2C_Canada_-_DSC08050.jpg/1024px-Greenheart_TreeWalk_-_UBC_Botanical_Garden_-_Vancouver%2C_Canada_-_DSC08050.jpg", // right click on the image at google image search and then select "Copy Image Address"
+        point: 650, // integer which is multiple of 50 (if required time to complete the quest from downtown is: ~30min => ~500pt, ~1hr => ~700pt, ~2hr => ~1000pt )
     })
 
     questRef.add({
-        quest_name: "Have a coffee at the top of Vancouver!",
-        location_name: "Lupin's Cafe - Grouse Mountain",
-        location: [49.37925579153738, -123.08358387425162],
-        rate: 5, //integer between 1 to 5
+        quest_name: "Nothing besides water!",
+        location_name: "Iona South Jetty",
+        location: [49.2092684568335, -123.26270005171358],
+        rate: 3, //integer between 1 to 5
         cost: 1, // integer between 1 to 3
-        description: "Located on the main floor of the Peak Chalet, Lupins Café offers stunning decorative alpine features like high cedar beams and copper lights that emit a soft glow in the evening.",
-        tag_ids: ["D4NeTY30U09lD3TemV4Y", "YFk0Z2I5rwxqH9ShsR8Z", "ntk5nt0wadhVx4A17Weu"],
-        image_name: "Lupin's Cafe",
+        description: "The Iona Jetty is a 8.2km long trail that juts out into the water. It is, in fact, a long pipe that runs out of the sewage treatment plant and into the water. If you were to view the jetty on a map, you will see a straight line in the water coming out of the peninsula. There are beaches in the area, although beachgoers and dogs are advised to avoid them because of the sewage emissions.",
+        tag_ids: ["D4NeTY30U09lD3TemV4Y", "ntk5nt0wadhVx4A17Weu"],
+        image_url: "https://www.insidevancouver.ca/wp-content/uploads/2021/11/ionajettyAlltrailsRickyBatallones-664x498.jpg",
+        point: 1500,
     })
 
     questRef.add({
-        quest_name: "Let's see the world's narrowest commercial building!",
-        location_name: "Sam Kee Building",
-        location: [49.28054048497626, -123.10476746176596],
-        rate: 2, //integer between 1 to 5
+        quest_name: "Welcome to the Forest of Books!",
+        location_name: "Vancouver Public Library",
+        location: [49.27982782965753, -123.11556849852188],
+        rate: 4, //integer between 1 to 5
         cost: 1, // integer between 1 to 3
-        description: "The Sam Kee Building at 8 West Pender Street is an internationally renowned narrow building, including a lower-level extension beneath the sidewalk, located at the traditional entry to Vancouver's historic Chinatown.",
-        tag_ids: ["j6MrxNTQVsVDqZZqKY3A"],
-        image_name: "Sam Kee Building",
+        description: "Vancouver Public Library (VPL) is the public library system for the city of Vancouver, British Columbia. In 2013, VPL had more than 6.9 million visits with patrons borrowing nearly 9.5 million items including: books, ebooks, CDs, DVDs, video games, newspapers and magazines. Across 22 locations and online, VPL serves nearly 428,000 active members and is the third-largest public library system in Canada.",
+        tag_ids: ["0o8PuZZQAbN2e6CU6veD", "j6MrxNTQVsVDqZZqKY3A"],
+        image_url: "https://www.vpl.ca/sites/default/files/styles/landscape_sm/public/branch-CEN.jpg?itok=Vpx3FFJd",
+        point: 350,
     })
 
     questRef.add({
-        quest_name: "Twisted tower - Can you believe it?",
-        location_name: "Vancouver House",
-        location: [49.27499326902219, -123.1310302325351],
-        rate: 2, //integer between 1 to 5
+        quest_name: "Enjoy exercise even on rainy days!",
+        location_name: "Vancouver Aquatic Centre",
+        location: [49.276949796368434, -123.13514559455747],
+        rate: 4, //integer between 1 to 5
         cost: 1, // integer between 1 to 3
-        description: "Vancouver House is a neo-futurist residential skyscraper in Vancouver, British Columbia, Canada. Construction of the skyscraper began in 2016 and was expected to be finished by the end of 2019, but completion was postponed to summer of 2020. On April 15, 2021, a water pipe on the 29th floor burst and damaged 17 units as well as several elevators.",
-        tag_ids: [""],
-        image_name: "Vancouver House",
+        description: "The Vancouver Aquatic Centre is a large city-run recreation centre with a swimming pool and fitness facilities. The swimming pool is a large indoor 50 metre pool with a diving tower (5m, 7.5m, and 10m), diving boards(1m and 3m), rope swing, and slide. It has a sauna and weight room.",
+        tag_ids: ["R5xMy6JZru7488KC6ICA"],
+        image_url: "https://images.dailyhive.com/20190513111503/vancouver-aquatic-centre.jpg",
+        point: 600,
     })
 }
