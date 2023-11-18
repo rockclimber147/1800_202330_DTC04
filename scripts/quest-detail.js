916a1a6fa720623e3c575c3bfd7539a2d45cada5
Daylen Smith <daylencsmith@gmail.com>
2023-11-18 15:14:28 -0800

    Make accept button show up only when quest is not accepted

--- scripts/quest-detail.js
+++ scripts/quest-detail.js
@@ -1,11 +1,10 @@
 async function display_quest_info() {
-
     await firebase.auth().onAuthStateChanged(async (user) => {
+
         let params = new URL(window.location.href);                              // get URL of search bar
         let ID = params.searchParams.get("quest_id");                            // get value for quest "id"
-        // Get both the user and quest docs
-        [user_doc, quest_doc] = await Promise.all([db.collection("users").doc(user.uid).get(),
-                                                   db.collection("quests").doc(ID).get()]);
+        // Get  quest doc
+        quest_doc = await db.collection("quests").doc(ID).get();
 
         var quest_name = quest_doc.data().quest_name;          // get the quest name
         var quest_rating = quest_doc.data().rate;              // get value of the "details" key
@@ -22,21 +21,60 @@ async function display_quest_info() {
         $('.quest_price').text('$'.repeat(quest_price));
         $(`.quest_description`).text(quest_description);
         $(`#complete_quest_pop_up .pop_up_confirm_button`).attr('onclick', `location.href='./quest-completion.html?quest_id=${ID}'`);
-    })
+
+        db.collection("users").doc(user.uid).onSnapshot((user_doc) => {
+            console.log("Current user data: ", user_doc.data());
+            let user_accepted_quests = user_doc.data().accepted_quests;
+            let user_completed_quests = user_doc.data().completed_quests;
+            console.log(`user accepted quests:`, user_accepted_quests);
+            console.log(`user completed quests:`, user_completed_quests);
+            let display_state;
+            if (user_accepted_quests.includes(ID)) {
+                display_state = 'quest is accepted';
+            } else if (user_completed_quests.includes(ID)) {
+                display_state = 'quest is completed';
+            } else {
+                display_state = 'quest is not yet accepted';
+            }
+            set_display_state(display_state)
+            switch_buttons_and_pop_ups(user, ID);
+        });
+    });
 }
 
-function switch_buttons_and_pop_ups() {
+function set_display_state(display_state) {
+    switch (display_state) {
+        case 'quest is accepted': {
+            $('#accept').addClass('d-none')
+            $('#drop').removeClass('d-none');
+            $('#complete').removeClass('d-none');
+            break;
+        } case 'quest is completed': {
+            $('#drop').addClass('d-none');
+            $('#complete').addClass('d-none');
+            $('#quest_is_completed').removeClass('d-none');
+            break;
+        } case 'quest is not yet accepted': {
+            $('#accept').removeClass('d-none')
+            break;
+        }
+    }
+}
+
+function switch_buttons_and_pop_ups(user, ID) {
     // when "Accept" is clicked
     $('.quest_accept_button').click(function () {
         $('#quest_accepted_pop_up').removeClass('d-none'); // pop-up shows up
         $('#details_go_here').addClass('opacity-25'); // quest detail fades
+        db.collection('users').doc(user.uid).update({
+            accepted_quests: firebase.firestore.FieldValue.arrayUnion(ID)
+        })
     })
 
     // when "Close" is clicked after accepting quest
     $('#quest_accepted_pop_up .pop_up_close_button').click(function () {
         $(this).closest('.pop_up').addClass('d-none'); // pop-up disappears
         $('.quest_accept_button').addClass('d-none'); // "Accept" button disappears
-        $('.quest_complete_button, .quest_drop_button').removeClass('d-none'); // "Complete" and "Drop" button show up
         $('#details_go_here').removeClass('opacity-25');
     })
 
@@ -82,5 +120,4 @@ function switch_buttons_and_pop_ups() {
 
 $(document).ready(function () {
     display_quest_info();
-    switch_buttons_and_pop_ups();
 })
