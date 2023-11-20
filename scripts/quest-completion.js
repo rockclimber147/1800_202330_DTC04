2ce18565c5a522d1136cbcc1f48dafb16510cc22
rockclimber147 <daylencsmith@gmail.com>
2023-11-20 14:04:11 -0800

    Format code

--- scripts/quest-completion.js
+++ scripts/quest-completion.js
@@ -1,3 +1,6 @@
+/**
+ * CHecks if user is logged in. Gets the quest id from the URL. Gets quest doc 
+ */
 async function do_all() {
     let user_doc;
     await firebase.auth().onAuthStateChanged(async user => {
@@ -7,7 +10,7 @@ async function do_all() {
 
             let params = new URL(window.location.href);                              // get URL of search bar
             let ID = params.searchParams.get("quest_id");                            // get value for quest "id"
-
+            // The following line waits for both the quest doc and the user doc at the same time!
             [user_doc, quest_doc] = await Promise.all([db.collection("users").doc(user.uid).get(),
                                                        db.collection("quests").doc(ID).get()]);
             console.log(`user_doc: ${user_doc}`)
@@ -28,6 +31,8 @@ async function do_all() {
             current_user_points += quest_point
             console.log(`User points after adding quest points: ${current_user_points}`)
 
+            current_user_level = user_doc.data().level
+
             db.collection('users').doc(user.uid).update({ 'points': current_user_points })
 
         } else {
@@ -35,8 +40,6 @@ async function do_all() {
             // window.location.href = 'index.html'; // Add this later so user must be logged in
         }
     })
-    
-    return user_doc;
 }
 
 $(document).ready(function () {
