74b0c6ee1f0685a5d1d747ded83fe75209990ccc
rockclimber147 <daylencsmith@gmail.com>
2023-11-27 15:43:39 -0800

    Add confirmation to quest creation

--- scripts/add_quest.js
+++ scripts/add_quest.js
@@ -4,9 +4,20 @@ async function add_quest_to_database() {
         // Check if user is signed in:
         if (user) {
             console.log(`user is logged in, user.uid: ${user.uid}`)
+            
+            // Make sure user is ok with the current state of the quest
+            let confirm_text = "Are you sure you want to add this quest?\nThis can't be undone!";
+            // return if user doesn't want to add quest
+            if (!confirm(confirm_text)){
+                return;
+            }
 
             
 
         } 
     })
+}
+
+function get_keywords_from_name(quest_name){
+    return quest_name.lower().split(' ');
 }
