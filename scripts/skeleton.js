48754d955a6cb7ae5b80c36b38edf400c97241fc
rockclimber147 <daylencsmith@gmail.com>
2023-11-27 14:16:29 -0800

    Move display_points_earned to skeleton.js

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -66,3 +66,16 @@ function loadSkeleton() {
 }
 loadSkeleton(); //invoke the function
 
+/*
+* Display user points on top navigation bar
+*/
+async function display_points_earned() {
+    let user_doc;
+    await firebase.auth().onAuthStateChanged(async user => {
+        if (user) {
+            user_doc = await db.collection("users").doc(user.uid).get();  // get user information
+            let user_point = user_doc.data().points;                  // get user points
+            $("#points_earned").text(`${user_point} pt`)            // display user points
+        }
+    })
+}
